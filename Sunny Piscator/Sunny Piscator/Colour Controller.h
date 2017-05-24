a9c4382ad60c5a297affb21cec7394f7b5f9f97d
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-05-24 21:16:25 +0300

    // TODO

---
+++ Sunny Piscator/Sunny Piscator/Colour Controller.h
@@ -0,0 +1,13 @@
+// Colour Controller.h - header file contains declaration of class for controlling colour
+#ifndef COLOUR_CONTROLLER_H
+#define COLOUR_CONTROLLER_H
+#include "stdafx.h"
+#include "Colour.h"
+#include "Cursor.h"
+
+class ColourController
+{
+	//TODO
+};
+
+#endif // !COLOUR_CONTROLLER_H
