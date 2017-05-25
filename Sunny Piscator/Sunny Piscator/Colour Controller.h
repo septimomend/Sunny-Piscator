1b6ba8ab4f05eef66a1ab8d25b841cf845e9278a
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-05-25 17:15:43 +0300

    added colour controller class declaration

--- Sunny Piscator/Sunny Piscator/Colour Controller.h
+++ Sunny Piscator/Sunny Piscator/Colour Controller.h
@@ -7,7 +7,20 @@
 
 class ColourController
 {
-	//TODO
+public:
+	ColourController(); // cstr
+	ColourController(Cursor* crs);
+	ColourController(int x, int y, Cursor* crs, ColourAppearance* clr);
+	void SetCoordinates(int x, int y); // sets coordinates
+	void SetCursor(Cursor* crs); // sets cursor position
+	void SetColour(colour back, colour fore); // sets colours
+	~ColourController(); // dstr 
+
+protected:
+	colour m_Back, m_Fore;
+	int m_X, m_Y;
+	Cursor* m_Crs; // cursor
+	ColourAppearance* m_Clr; // colour
 };
 
 #endif // !COLOUR_CONTROLLER_H
