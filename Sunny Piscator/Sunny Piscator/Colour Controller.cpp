42bd37cf65e8412a592b70d2816b2c8934ebbbc8
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-05-26 18:16:41 +0300

    add colour/cursor setting

--- Sunny Piscator/Sunny Piscator/Colour Controller.cpp
+++ Sunny Piscator/Sunny Piscator/Colour Controller.cpp
@@ -4,4 +4,47 @@
 
 using namespace std;
 
-// TODO
+ColourController::ColourController() // cstr
+{ 
+	m_Crs = 0; 
+	m_Clr = 0; 
+	m_Back = White; 
+	m_Fore = White; 
+}
+
+ColourController::ColourController(Cursor* crs) : m_Crs(crs) // cstr 2
+{ 
+}
+
+ColourController::ColourController(int x, int y, Cursor* crs, ColourAppearance * clr) // cstr 3
+{ 
+	m_X = x;
+	m_Y = y;
+	m_Crs = crs;
+	m_Clr = clr;
+}
+
+void ColourController::SetCoordinates(int x, int y)
+{
+	m_X = x;
+	m_Y = y;
+}
+
+void ColourController::SetCursor(Cursor* crs) 
+{
+	m_Crs = crs;
+}
+
+void ColourController::SetColour(colour back, colour fore) 
+{
+	m_Back = back;
+	m_Fore = fore;
+}
+
+ColourController::~ColourController() 
+{ 
+	if (m_Clr) 
+		delete[] m_Clr; 
+	if (m_Crs) 
+		delete[] m_Crs; 
+}
