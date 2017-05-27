cb1c3f656d2577168f99a21085eca87a83074fe4
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-05-27 16:49:45 +0300

    game's visual interface

--- Sunny Piscator/Sunny Piscator/Shape.cpp
+++ Sunny Piscator/Sunny Piscator/Shape.cpp
@@ -2,4 +2,37 @@
 #include "stdafx.h"
 #include "Shape.h"
 
-// TODO
+/*
+* cstr
+*/
+Shape::Shape(int x, int y) : m_PosX(x), m_PosY(y)
+{  
+	m_pCrs = 0; 
+	m_pClr = 0; 
+}
+
+/*
+* set position
+*/
+void Shape::SetPosition(int x, int y) 
+{ 
+	m_PosX = x;
+	m_PosY = y;
+}
+
+/*
+* set cursor and colour controllers
+*/
+void Shape::SetController(Cursor* crs_x, ColourAppearance* clr_x)
+{
+	m_pCrs = crs_x;
+	m_pClr = clr_x;
+}
+
+/*
+* set background colour
+*/
+void Shape::SetColours(colour back_clr) 
+{ 
+	m_Back = back_clr;
+}
--- Sunny Piscator/Sunny Piscator/Shape.h
+++ Sunny Piscator/Sunny Piscator/Shape.h
@@ -7,7 +7,22 @@
 
 class Shape
 {
-	//TODO
+public:
+	Shape(int x, int y); // cstr
+	void SetPosition(int x, int y); // sets coordinate position
+	void SetController(Cursor* crs_x, ColourAppearance* clr_x); // sets cursor and colour
+	void SetColours(colour back_clr); // sets background colour
+	void DrawTop(int num); // draws top (hat)
+	void DrawSquare(int num); // draws game's square
+	void DrawRectangle(int width, int height); // draws rectangle
+	void DrawNewcamer(); // draws new player
+	void MoveAxisX(int x); // moving at X axis for player
+
+private:
+	int m_PosX, m_PosY, m_Temp;
+	Cursor* m_pCrs;
+	ColourAppearance* m_pClr;
+	colour m_Back;
 };
 
 #endif // !SHAPE_H
