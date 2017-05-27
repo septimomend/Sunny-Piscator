648891079ae40645bf65ce15a0124528a0abd24c
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-05-27 17:01:38 +0300

    indicator // TODO

---
+++ Sunny Piscator/Sunny Piscator/Indicator.cpp
@@ -0,0 +1,5 @@
+// Indicator.cpp - definition of class, placed in Indicator.h
+#include "stdafx.h"
+#include "Indicator.h"
+
+// TODO
---
+++ Sunny Piscator/Sunny Piscator/Indicator.h
@@ -0,0 +1,13 @@
+// Indicator.h - header file contains declaration of class for describing counter
+#ifndef INDICATOR_H
+#define INDICATOR_H
+#include "stdafx.h"
+#include "Colour.h"
+#include "Cursor.h"
+
+class Indicator
+{
+	// TODO
+};
+
+#endif // !INDICATOR_H
