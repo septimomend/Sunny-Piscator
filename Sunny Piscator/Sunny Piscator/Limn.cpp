36e9772e9d95fef1e761cb17cf0fc2e89e1ae7ce
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-05-30 16:52:40 +0300

    limn interface // TODO

---
+++ Sunny Piscator/Sunny Piscator/Limn.cpp
@@ -0,0 +1,5 @@
+// Limn.cpp - definition of class, placed in Limn.h
+#include "stdafx.h"
+#include "Limn.h"
+
+// TODO
---
+++ Sunny Piscator/Sunny Piscator/Limn.h
@@ -0,0 +1,11 @@
+// Limn.h - header file contains declaration of class for limning the border of game and game interface
+#ifndef LIMN_H
+#define LIMN_H
+#include "stdafx.h"
+#include "Colour Controller.h"
+#include "Colour.h"
+#include "Cursor.h"
+
+// TODO
+
+#endif // !LIMN_H
