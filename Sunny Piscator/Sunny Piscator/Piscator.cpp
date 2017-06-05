853e7341ded1d2602cf47d236dd19b2834ee56d3
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-06-05 16:21:41 +0300

    piscator // TODO

---
+++ Sunny Piscator/Sunny Piscator/Piscator.cpp
@@ -0,0 +1,7 @@
+// Piscator.cpp - definition of class, placed in Piscator.h
+#include "stdafx.h"
+#include "Piscator.h"
+
+using namespace std;
+
+// TODO
---
+++ Sunny Piscator/Sunny Piscator/Piscator.h
@@ -0,0 +1,13 @@
+// Piscator.h - header file contains declaration of class that describes main game's object - piscator
+#ifndef PISCATOR_H
+#define PISCATOR_H
+#include "stdafx.h"
+#include "Colour.h"
+#include "Cursor.h"
+#include "Limn.h"
+
+class Piscator : public Limn
+{
+	// TODO
+};
+#endif // !PISCATOR_H
